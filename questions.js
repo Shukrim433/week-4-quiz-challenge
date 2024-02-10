35975077f92afb97d3dbc788cd2f7505bbc64cf6
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-02-10 15:01:57 +0000

    added the questions to an object in the questions.js file

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question : 'Commonly used data types DO NOT include:',
+        options : ['strings', 'booleans', 'alerts', 'numbers'],
+        answer : 'alerts'
+    },
+    {
+        question : 'The condition in an if / else statement is enclosed within:',
+        options : ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+        answer : 'parentheses'
+    },
+    {
+        question : 'Arrays in javascript can be used to score:',
+        options : ['numbers and strings', 'booleans', 'other arrays', 'all of the above'],
+        answer : 'all of the above'  
+    },
+    {
+        question : 'String values must be enclosed within _____ when being assigned to variables:',
+        options : ['quotes', 'curly brackets', 'parentheses', 'commas'],
+        answer : 'quotes'
+    },
+    {
+        question : 'A very useful tool using development and debugging for printing content to the debugger is:',
+        options : ['javascript', 'console.log', 'for loops', '.createElement'],
+        answer : 'convole.log'
+    }
+]
